fdf9adf4842ef6701049b893ea36f9258776ca69
Swifthing <hannyce@icloud.com>
2022-07-29 15:49:47 +0200

    create big burger model

---
+++ BigBurger/Model/BigBurgerModel.swift
@@ -0,0 +1,16 @@
+//
+//  BigBurgerModel.swift
+//  BigBurger
+//
+//  Created by Anis on 29/07/2022.
+//
+
+import Foundation
+
+struct BigBurgerModel: Codable {
+    var ref: String
+    var title: String
+    var description: String
+    var thumbnail: String
+    var price: Int
+}
