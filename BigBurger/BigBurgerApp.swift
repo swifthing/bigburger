999e78b0fb78a2bd6d549796bda6c7b17e96e54f
Swifthing <hannyce@icloud.com>
2022-07-29 15:27:52 +0200

    first commit

---
+++ BigBurger/BigBurgerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BigBurgerApp.swift
+//  BigBurger
+//
+//  Created by Anis on 29/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct BigBurgerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
